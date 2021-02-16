e705484c7e4d8f25dd34fcf4b93ac39190c97a58
todorovapetyaa <57499719+todorovapetyaa@users.noreply.github.com>
2021-02-16 22:17:17 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -57,8 +57,12 @@ gulp.task("watch", function() {
 });
 
 
-gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
-}));
+// gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
+// 	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
+// }));
 
 gulp.task("default", gulp.series('jekyll', 'sass', 'watch'));
+
+gulp.task("deploy", function() {
+	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
+});
